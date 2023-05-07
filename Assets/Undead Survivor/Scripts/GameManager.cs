986682a0ae6c013c32077511ab7d8cabbda4514e
mekain80 <rhfnajfl0@naver.com>
2023-05-08 02:40:16 +0900

    ver 1.10
    HUD //It's a bit of a mess.

--- Assets/Undead Survivor/Scripts/GameManager.cs
+++ Assets/Undead Survivor/Scripts/GameManager.cs
@@ -10,6 +10,9 @@ public class GameManager : MonoBehaviour
     public float gameTime;
     public float maxGameTime = 2 * 10f;
     [Header("# Player Info")]
+    public int health;
+    public int maxHealth = 100;
+
     public int level;
     public int kill;
     public int exp;
@@ -22,6 +25,13 @@ public class GameManager : MonoBehaviour
     void Awake()
     {
         instance = this;
+    }
+
+    void Start()
+    {
+        maxHealth = 100;
+        health = maxHealth;
+        Debug.Log(maxHealth);
     }
 
     void Update()
