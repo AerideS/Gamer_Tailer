16e20854b50498cc4f40528808c78ff9f65ed48c
lemonmungchi <khc112353@gmail.com>
2023-11-06 00:39:19 +0900

    스테이지및 적추가
    스테이지및 적추가

--- Assets/Undead Survivor/Scripts/Spawner.cs
+++ Assets/Undead Survivor/Scripts/Spawner.cs
@@ -33,10 +33,10 @@ public class Spawner : MonoBehaviour
 
     }
 
-    void spawn()
+    void spawn()                        //0번째부터시작 1,2번째는 총알이라 놔두기 
     {
         GameObject enemy;
-        if (GameManager.instance.stageIndex == 0)
+        if (GameManager.instance.stageIndex == 0)           //1스테이지는 레벨에 따라
         {
             if (level == 0)
             {
@@ -50,10 +50,10 @@ public class Spawner : MonoBehaviour
             enemy.GetComponent<Enemy_new>().Init(spawnData[level]);
         }
         else
-        {
+        {                                                           //2스테이지부터는 레벨+스테이지 인덱스번째의 몹부터 소환
             if (level == 0)
             {
-                enemy = GameManager.instance.pool.Get(level+1+GameManager.instance.stageIndex);    //SpawnData 인스펙터창
+                enemy = GameManager.instance.pool.Get(level+2+GameManager.instance.stageIndex);    //SpawnData 인스펙터창
             }
             else
             {
